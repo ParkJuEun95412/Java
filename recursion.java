3d625fccbea87330e83ca3e05bbb600c54b7fb23
ParkJuEun95412 <57995077+ParkJuEun95412@users.noreply.github.com>
2020-06-22 18:00:59 +0900

    algorithm_3

--- recursion.java
+++ recursion.java
@@ -187,3 +187,109 @@ public void readFrom(int n , int [] data, Scanner in){
 - 순환함수는 복잡한 알고리즘을 단순하고 알기쉽게 표현하는 것을 가능하게 함
 - 하지만 함수 호출에 따른 오버헤드가 있음 (매개변수 전달, 액티베이션 프레임 생성 등)
 */
+
+  /* 순환적 알고리즘 설계
+- 적어도 하나의 base case, 즉 순환되지 않고 종료되는 case가 있어야 함
+- 모든 case는 결국 base case로 수렴해야 함
+*/
+
+/* 암시적(implicit) 매개변수를 명시적(explicit) 매개변수로 바꾸어라 */
+
+// 순차 탐색
+
+/* 함수의 미션은 data[0]에서 data[n-1] 사이에서 target 을 검색하는 것. 하지만 검색 구간의 시작 인덱스 0은 보통 생략
+즉, 암시적 매개변수 */
+
+int search(int [] data, int n, int target) {
+  for (int i=0; i<n; i++)
+    if (data[i]==target)
+      return i;
+  return -1;
+}
+
+// 매개변수의 명시화: 순차 탐색
+
+/* 함수의 미션은 data[begin]에서 data[end] 사이에서 target을 검색. 즉, 검색 구간의 시작점을 명시적(explicit)으로 지정
+이 함수를 search(data, 0, n-1, target)으로 호출한다면 앞 페이지의 함수와 완전히 동일한 일을 수행 */
+
+int search(int [] data, int begin, int end, int target) {
+    if (begin>end) //데이터0, begin==end 데이터 한개
+        return -1;
+    else if (target=items[begin])
+        return begin;
+    else
+        return search(data, begin+1, end, target);
+}
+
+/* 순차 탐색: 다른 버전
+data[begin]에서 data[end] 사이에서 target을 검색. 즉, 검색구간의 시작점을 명시적(explicit)으로 지정
+*/
+
+int search(int [] data, int begin, int end, int target) {
+    if (begin>end) //데이터0, begin==end 데이터 한개
+        return -1;
+    else if (target=items[end])
+        return end;
+    else
+        return search(data, begin, end-1, target);
+}
+
+int search(int [] data, int begin, int end, int target) {
+    if (begin>end) //데이터0, begin==end 데이터 한개
+        return -1;
+    else {
+        int middle = (begin+end)/2;
+        if (items[middle]==target)
+            return middle;
+        int index = search(data, begin, middle-1,target);
+        if (index != -1)
+            return index;
+        else
+            return search(data, middle+1, end, target);
+    else
+        return search(data, begin, end-1, target);
+    }
+}// binary search와는 다름
+
+
+/* 매개변수의 명시화: 최대값 찾기
+data[begin]에서 data[end] 사이에서 최대값을 찾아 변환한다. begin<=end라고 가정 
+*/
+
+int findMax(int [] data, int begin, int end) {
+    if (begin==end)
+        return data[begin];
+    else
+        return Math.max(data[begin], findMax(data, begin+1, end));
+}
+
+// 최대값 찾기: 다른 버전
+int findMax(int [] data, int begin, int end) {
+    if (begin==end)
+        return data[begin];
+    else
+        int middle=(begin+end)/2;
+        int max1=findMax(data,begin,middle);
+        int max2=findMax(data, middle+1,end);
+        return Math.max(max1,max2));
+}
+
+
+/* Binart Search
+items[begin]에서 items[end]사이에서 target을 검색
+*/
+
+public static int binarySearch(String[] items, String target, int begin, int end) {
+    if (begin>end)
+        return -1;
+    else
+        int middle = (begin+end)/2;
+        int compResult=target.compareTo(items[middle]);
+        if (compResult ==0)
+            return middle;
+        else if (compResult<0)
+            return binarySearch(items, target, begin, middle-1);
+        else
+            return binarySearch(items, target, middle+1,end);
+    }
+}
